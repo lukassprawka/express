55ba558bf801f62df7345025465ab9da59f82150
Lukasz Sprawka <lukas.sprawka@gmail.com>
2020-01-27 15:12:57 +0100

    login

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: 'mongodb+srv://admin:BynpxCAcLnDaGi9O@cluster0-9jt7s.mongodb.net/test?retryWrites=true&w=majority',
+  keysSession: ['TWOJKLUCZ'],
+  maxAgeSession: 24 * 60 * 60 * 1000
+}
