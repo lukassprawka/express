efa8dc68d4f4a35e97d50e85092452a8daf8b05b
Lukasz Sprawka <lukas.sprawka@gmail.com>
2020-01-29 10:46:48 +0100

    admin news

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var config = require('./config');
+const mongoose = require('mongoose');
+mongoose.connect(config.db, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const News = require('../models/news');
 const router = express.Router();
 
 router.all('*', (req, res, next) => {
@@ -13,9 +14,37 @@ router.all('*', (req, res, next) => {
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  console.log(req.session.admin);
+  News.find({}, (err, data) => {
+    console.log(data);
 
-  res.render('admin', { title: 'Admin' });
+    res.render('admin/index', { title: 'Admin', data });
+  });
+});
+
+router.get('/news/add', (req, res) => {
+  res.render('admin/news-form', { title: 'Dodaj news', errors: {}, body: {} });
+});
+
+router.post('/news/add', (req, res) => {
+  const body = req.body;
+
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+
+  newsData.save((err) => {
+    if (err) {
+      res.render('admin/news-form', { title: 'Dodaj news', errors, body });
+      return;
+    }
+
+    res.redirect('/admin')
+  });
+});
+
+router.get('/news/delete/:id', (req, res) => {
+  News.findByIdAndDelete(req.params.id, (err) => {
+    res.redirect('/admin');
+  });
 });
 
 module.exports = router;
